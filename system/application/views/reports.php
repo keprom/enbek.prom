4b433ea29b82cd361ac9bc390d1caca9bc280ef0
keprom <kokshetau.energo.prom@mail.ru>
2018-12-28 09:13:21 +0600

    added kontragent rek

--- system/application/views/reports.php
+++ system/application/views/reports.php
@@ -43,3 +43,4 @@ echo anchor("billing/pre_ip_obshiy","Частники, расчитывающи�
 <li><a href="<?php echo site_url('billing/pre_fine_2_re'); ?>"><?php echo '2-РЭ (пеня)'; ?></a></li>
 <li><a href="<?php echo site_url('billing/pre_fine_7_re'); ?>"><?php echo '7-РЭ (пеня)'; ?></a></li>
 <li><a href="<?php echo site_url('billing/pre_svod_oplat_po_firmam_year'); ?>"><?php echo 'Годовой свод оплат в разрезе фирм'; ?></a></li>
+<li><a href="<?php echo site_url('billing/kontragent_rek'); ?>"><?php echo 'Список контрагентов с реквизитами'; ?></a></li>
